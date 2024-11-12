3954efb2c032a927dcfea5675c01e1e5452466a5
Bhavesh Patil <bhaveshspatil6910@gmail.com>
2024-11-12 22:33:57 +0530

    Quick sort V3

--- quick.c
+++ quick.c
@@ -38,7 +38,7 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5};
+    int arr[] = {1, 17, 88, 64, 14, 65};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     quickSort(arr, 0, n - 1);
